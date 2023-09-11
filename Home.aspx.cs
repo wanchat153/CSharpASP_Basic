fbce411b6646c909ff352643c2a03036354cffed
M153 <wanchatgame@gmail.com>
2023-09-11 22:07:52 +0700

    Cookie

--- Home.aspx.cs
+++ Home.aspx.cs
@@ -11,7 +11,18 @@ namespace CSharpASP_Basic
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (ReadCookie() > 15)
+            {
+                //เปลี่ยนหน้า
+                Response.Redirect("Member.aspx");
 
+                //Redirect ผ่าน Server
+                //Server.Transfer("Member.aspx");
+            }
+            else
+            {
+                Response.Write("Error");
+            }
         }
 
         protected void Button1_Click(object sender, EventArgs e)
@@ -22,18 +33,35 @@ namespace CSharpASP_Basic
             int years = DateTime.Now.Year;
             //นำ years มาลบกับ Text
             int age = years - int.Parse(TextBox1.Text);
-            if (age > 15)
+
+            WriteCooke(age);
+
+            if (ReadCookie() > 15)
             {
                 //เปลี่ยนหน้า
-                //Response.Redirect("Member.aspx");
+                Response.Redirect("Member.aspx");
 
                 //Redirect ผ่าน Server
-                Server.Transfer("Member.aspx");
+                //Server.Transfer("Member.aspx");
             }
             else
             {
                 Response.Write("Error");
             }
+        }
+
+        private void WriteCooke(object age)
+        {
+            HttpCookie userCookie = new HttpCookie("UserSetting");
+            userCookie.Value = age.ToString();
+            userCookie.Expires = DateTime.Now.AddDays(3d);
+            Response.Cookies.Add(userCookie);
+        }
+
+        private int ReadCookie()
+        {
+            HttpCookie userCookie = Request.Cookies["UserSetting"];
+            return int.Parse(userCookie.Value);
         }
 
         protected void TextBox1_TextChanged(object sender, EventArgs e)
